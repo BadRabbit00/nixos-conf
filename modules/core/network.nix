1e4bdc973264b7b28b374fac7b886cc6b1d6e2fc
BadRabbit <alexeyb06@gmail.com>
2025-12-12 22:58:44 +0500

    Update hostname in network configuration to match local setup

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  networking.hostName = "nixos"; # We can change this per host if needed
+  networking.hostName = "badrabbitpc"; # We can change this per host if needed
   networking.networkmanager.enable = true;
   
   # Firewall settings if needed
